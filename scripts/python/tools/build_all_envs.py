21c7e4f0f56e23901bc5979c9335caf80e23cb58
Duisterhof <b.p.duisterhof@student.tudelft.nl>
2020-10-18 19:48:15 +0200

    updated point spawner

--- scripts/python/tools/build_all_envs.py
+++ scripts/python/tools/build_all_envs.py
@@ -5,5 +5,6 @@ from randomize_environment import get_spawn_pos
 parser = argparse.ArgumentParser(description='Build initial positions for all environments')
 parser.add_argument('folder', type=str, help="(str) Folder with envs")
 parser.add_argument('n_agents',type=int,help="(int) number of agents")
+parser.add_argument('min_dist',type=float,help="(float) min distance between points, to make sure multiple agents don't collide")
 args = parser.parse_args()
-get_spawn_pos(args.n_agents,args.folder)
+get_spawn_pos(args.n_agents,args.folder, args.min_dist)
